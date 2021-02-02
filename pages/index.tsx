ac78de13cd3faf9735a0fed87e63551b582d9784
温翔宇 <fishfly516@gmail.com>
2021-02-02 14:00:41 +0800

    change layout to _app

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,11 @@
 import Head from "next/head";
-import Layout from "../components/layout";
-import { RecoilRoot } from 'recoil';
-import Content from '../components/content';
 
 export const siteTitle = "三两首页";
 
 export default function Home() {
-
   return (
-    <RecoilRoot>
-      <Layout>
-        <Head>
-          <title>{siteTitle}</title>
-        </Head>
-        <Content />
-      </Layout>
-    </RecoilRoot>
+    <Head>
+      <title>{siteTitle}</title>
+    </Head>
   );
 }
