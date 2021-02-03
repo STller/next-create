3d3eff4f02801f0c676de03a877b7b6740e04e53
温翔宇 <fishfly516@gmail.com>
2021-02-03 09:31:22 +0800

    detect orientation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -24,13 +24,13 @@ export default function MyApp({ Component, pageProps }) {
     window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", function() {
       if (window.orientation === 180 || window.orientation === 0) { 
         const { clientHeight } = document.documentElement;
-        // setSectionHeight(clientHeight - 92);
+        setSectionHeight(clientHeight - 92);
     
         alert(clientHeight);
       } 
       if (window.orientation === 90 || window.orientation === -90 ){ 
         const { clientHeight } = document.documentElement;
-        // setSectionHeight(clientHeight - 92);
+        setSectionHeight(clientHeight - 92);
     
         alert(clientHeight);
       } 
